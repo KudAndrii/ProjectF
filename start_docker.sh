fbfcb637f49f1d6398113f7dafe3c97ea48d23ca
KudAndrii <cud.andrei@gmail.com>
2025-01-01 18:56:41 +0200

    wip.

--- start_docker.sh
+++ start_docker.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Drop existing containers and images related to the yaml file
-docker-compose down --rmi local
-
-# Source the .env file
+# Source the .env file to get $SECRETS_FILE_PROJECT_F
 source .env
 
 # Load secrets from secrets.json
 if [[ -f "$SECRETS_FILE_PROJECT_F" ]]; then
-    export OMDB_API_KEY=$(jq -r '.OmdbConfiguration.ApiKey' "$SECRETS_FILE_PROJECT_F")
+    OMDB_API_KEY=$(jq -r '.OmdbConfiguration.ApiKey' "$SECRETS_FILE_PROJECT_F")
+    export OMDB_API_KEY
 else
     echo "Secrets file not found at $SECRETS_FILE_PROJECT_F"
     exit 1
 fi
+
+# Drop existing containers and images related to the yaml file
+docker-compose down --rmi local
 
 # Run named docker-compose with loaded variables
 docker-compose -p project-f up --build -d
